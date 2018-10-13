841f801c3cde9019d4408368519b8b7f5ae1e68e
zzamny1013 <msyh0207@naver.com>
2018-10-14 00:41:44 +0900

    hey

--- assignment.js
+++ assignment.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
 
-fs.writeFile('./1.txt', '11111', function(err) {
-    console.log('1.txt created');
-    fs.readFile('./1.txt','UTF-8', function(err,data1) {
-        fs.writeFile('./2.txt',data1 + '22222', function(err) {
-            console.log('2.txt created');
-            fs.readFile('./2.txt','UTF-8', function(err,data2) {
-                fs.writeFile('./3.txt',data2 + '33333', function(err) {
-                    console.log('3.txt created');
-                    fs.readFile('./3.txt','UTF-8', function(err,data3) {
-                        console.log(data3);
-                    });
-                });
-            });
+function kkk(){
+    return new Promise(function (resolve, reject){
+        fs.writeFile('./.txt', '11111', function(response){
+            if(response){
+                console.log('1.txt created');
+                resolve(response);
+            }
+            reject(new Error("Request is failed"));
         });
     });
-});
+}
 
+kkk()
+.then(function(){fs.readFile('./1.txt', 'UTF-8');})
+.catch(function(err, data1){fs.writeFile('./2.txt', data1 + '22222');})
+.catch(function(err){console.log('2 txt created');})
+
+.then(function(){fs.readFile('./2.txt', 'UTF-8');})
+.catch(function(err,data2){fs.writeFile('./3.txt', data2+'33333');})
+.catch(function(err, data3){console.log(data3);})
